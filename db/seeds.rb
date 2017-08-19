4eac8f84d17ecd874a1cf0876770cad4a0c55011
kinkorea <alexkarasik89@gmail.com>
2017-08-18 22:24:45 -0400

    added a schema and example data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# 10,times do |proposal|
-#   Proposal.create! (
-#    customer: "Customer #{proposal}",
-#    portfolio_url: 'http://alexkarasik.com',
-#    tools: 'Ruby on Rails. Angular 2 and Postgres',
-#    estimated_hours: (80 + proposal),
-#    hourly_rate: 120,
-#    weeks_to_complete: 12,
-#    client_email: 'alexkarasik89@gmail.com',
-#   )
-# end
+
+10.times do |proposal|
+	Proposal.create!(
+		customer: "Customer #{proposal}",
+		portfolio_url: 'http://alexkarasikcom',
+		tools: 'Ruby on Rails, Angular 2, and Postgres',
+		estimated_hours: (80 + proposal),
+		hourly_rate: 120,
+		weeks_to_complete: 12,
+		client_email: 'alexkarasik89@gmail.com',
+	)
+end
